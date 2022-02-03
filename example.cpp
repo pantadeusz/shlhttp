437676cdbcdab915eac586a260b4ac385e4e42db
pantadeusz <none@none.com>
2022-02-03 22:13:12 +0100

    minor usability fix and formatting

--- example.cpp
+++ example.cpp
@@ -1,15 +1,36 @@
-#include "shlhttp.hpp"
+#include "shttp.hpp"
 #include <iostream>
 
+const std::string GET_RESPONSE_TEXT = R"(<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>SHTTP</title>
+</head>
+<body>
+<h2>Hello shttp</h2>
+ <FORM action="/"
+       enctype="multipart/form-data"
+       method="post">
+   <P>
+   What is your name? <INPUT type="text" name="submit-name"><BR>
+   What files are you sending? <INPUT type="file" name="files"><BR>
+   <INPUT type="submit" value="Send"> <INPUT type="reset">
+ </FORM>
+</body>
+</html>
+)";
+
 int main() {
-  using namespace shlhttp;
+  using namespace shttp;
   int lsocket = listen_socket();
   do {
     auto [acc_socket, client_host, client_port] = accept_on_socket(lsocket);
     std::cout << acc_socket << " " << client_host << " " << client_port
               << std::endl;
     auto request = receive_request(acc_socket);
-    response_t response = response_of(200,"well done","Witaj");
+    std::cout << request << std::endl;
+    response_t response = response_of(200,"ok",GET_RESPONSE_TEXT);
     send_response(acc_socket, response);
     close(acc_socket);
   } while (true);
--- test.cpp
+++ test.cpp
@@ -1,59 +1,59 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "shlhttp.hpp"
+#include "shttp.hpp"
 
 #include <sstream>
 
-using namespace shlhttp;
+using namespace shttp;
 
-TEST_CASE("request_t", "[request_t]") {
-  auto request = std::make_shared<request_t>();
-  request->method = "GET";
-  request->path = "/";
-  request->version = "HTTP/1.1";
-  request->headers["Host"] = "localhost:5000";
-  request->body = {};
-  SECTION("request_to_data generates correct data vector") {
-    std::stringstream data_to_send;
-    data_to_send << *request;
-    std::string string_to_send = data_to_send.str();
-    REQUIRE(string_to_send == "GET / HTTP/1.1\r\nhost: localhost:5000\r\n\r\n");
-  }
+TEST_CASE( "request_t", "[request_t]" ) {
+	auto request = std::make_shared<request_t>();
+	request->method = "GET";
+	request->path = "/";
+	request->version = "HTTP/1.1";
+	request->headers["Host"] = "localhost:5000";
+	request->body = {};
+	SECTION( "request_to_data generates correct data vector" ) {
+		std::stringstream data_to_send;
+		data_to_send << *request;
+		std::string string_to_send = data_to_send.str();
+		REQUIRE( string_to_send == "GET / HTTP/1.1\r\nhost: localhost:5000\r\n\r\n" );
+	}
 
-  SECTION("two requests can be compared") {
-    auto request2 = std::make_shared<request_t>();
-    request2->method = "GET";
-    request2->path = "/";
-    request2->version = "HTTP/1.1";
-    request2->headers["Host"] = "localhost:5000";
-    request2->body = {};
-    REQUIRE(*request == *request2);
+	SECTION( "two requests can be compared" ) {
+		auto request2 = std::make_shared<request_t>();
+		request2->method = "GET";
+		request2->path = "/";
+		request2->version = "HTTP/1.1";
+		request2->headers["Host"] = "localhost:5000";
+		request2->body = {};
+		REQUIRE( *request == *request2 );
 
-    *request2 = *request;
-    request2->method = "POST";
-    REQUIRE_FALSE(*request == *request2);
-    *request2 = *request;
-    request2->path = "/xx";
-    REQUIRE_FALSE(*request == *request2);
-    *request2 = *request;
-    request2->version = "HTTP/1.0";
-    REQUIRE_FALSE(*request == *request2);
-    *request2 = *request;
-    request2->headers.clear();
-    REQUIRE_FALSE(*request == *request2);
-    *request2 = *request;
-    request2->headers["xx"] = "dsdf";
-    REQUIRE_FALSE(*request == *request2);
-    *request2 = *request;
-    request2->body = {'d', 'w'};
-    REQUIRE_FALSE(*request == *request2);
-  }
+		*request2 = *request;
+		request2->method = "POST";
+		REQUIRE_FALSE( *request == *request2 );
+		*request2 = *request;
+		request2->path = "/xx";
+		REQUIRE_FALSE( *request == *request2 );
+		*request2 = *request;
+		request2->version = "HTTP/1.0";
+		REQUIRE_FALSE( *request == *request2 );
+		*request2 = *request;
+		request2->headers.clear();
+		REQUIRE_FALSE( *request == *request2 );
+		*request2 = *request;
+		request2->headers["xx"] = "dsdf";
+		REQUIRE_FALSE( *request == *request2 );
+		*request2 = *request;
+		request2->body = {'d', 'w'};
+		REQUIRE_FALSE( *request == *request2 );
+	}
 
-  SECTION("generate request from string") {
-    std::string req_str = "GET / HTTP/1.1\r\nhost: localhost:5000\r\n\r\n";
-    std::stringstream ss(req_str);
-    request_t req;
-    ss >> req;
-    std::cout << req;
-  }
+	SECTION( "generate request from string" ) {
+		std::string req_str = "GET / HTTP/1.1\r\nhost: localhost:5000\r\n\r\n";
+		std::stringstream ss( req_str );
+		request_t req;
+		ss >> req;
+		std::cout << req;
+	}
 }
